f3e94700e3385041ce7f5e61c9d8ed403b0ca7d2
ayoubmez96 <ayoubmez1996@gmail.com>
2022-03-13 13:21:33 +0100

    setup calculate winner logic

--- tic-tac-toe-front/src/components/Board/index.tsx
+++ tic-tac-toe-front/src/components/Board/index.tsx
@@ -1,6 +1,60 @@
-import React from 'react';
+import React from 'react'
+
+type Board = {
+  0: string | null,
+  1: string | null,
+  2: string | null,
+  3: string | null,
+  4: string | null,
+  5: string | null,
+  6: string | null,
+  7: string | null,
+  8: string | null
+}
+
+const DEFAULT_BOARD = {
+  0: null,
+  1: null,
+  2: null,
+  3: null,
+  4: null,
+  5: null,
+  6: null,
+  7: null,
+  8: null
+}
 
 const Board: React.FC = () => {
+  const [squares, setSquares] = React.useState<Board>(DEFAULT_BOARD)
+
+  const calculateStatus = (winner: string | null) => {
+    if (winner) return `Winner: ${winner}`
+  }
+
+  let restart = () => {
+    setSquares(DEFAULT_BOARD)
+  }
+
+  let calculateWinner = (squares: any) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+    return null
+  }
+
   return (
     <div>
       <div>
